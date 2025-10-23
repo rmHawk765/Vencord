b4483e460f192438d994c316c66f1b96edd6fe9f
rmHawk765 <171146113+rmHawk765@users.noreply.github.com>
2025-10-23 17:43:32 +0100

    add rmHawk765 as contributor

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -606,6 +606,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "thororen",
         id: 848339671629299742n
     },
+    rmHawk765: {
+        name: "rmHawk765",
+        id: 1208535402747527238n
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
